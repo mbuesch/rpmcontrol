efbd28bdc0062d1724554ec84b41da58caa2dfa7
Michael Büsch <m@bues.ch>
2025-09-08 21:07:07 +0200

    filter: Use big fixpt

--- firmware/src/filter.rs
+++ firmware/src/filter.rs
@@ -1,33 +1,35 @@
 use crate::{
-    fixpt::{Fixpt, fixpt},
+    fixpt::{BigFixpt, Fixpt, fixpt},
     mutex::{MainCtx, MutexCell},
 };
 
 pub struct Filter {
-    buf: MutexCell<Fixpt>,
+    buf: MutexCell<BigFixpt>,
 }
 
 impl Filter {
     pub const fn new() -> Self {
         Self {
-            buf: MutexCell::new(fixpt!(0)),
+            buf: MutexCell::new(fixpt!(0).upgrade()),
         }
     }
 
     pub fn reset(&self, m: &MainCtx<'_>) {
-        self.buf.set(m, fixpt!(0));
+        self.buf.set(m, fixpt!(0).upgrade());
     }
 
+    #[inline(never)]
     pub fn run(&self, m: &MainCtx<'_>, input: Fixpt, div: Fixpt) -> Fixpt {
+        let div: BigFixpt = div.into();
         let mut buf = self.buf.get(m);
         buf -= buf / div;
-        buf += input;
+        buf += input.into();
         self.buf.set(m, buf);
-        buf / div
+        (buf / div).into()
     }
 
     pub fn get(&self, m: &MainCtx<'_>, div: Fixpt) -> Fixpt {
-        self.buf.get(m) / div
+        (self.buf.get(m) / div.into()).into()
     }
 }
 
