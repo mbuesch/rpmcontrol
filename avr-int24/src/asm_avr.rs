2fca9407276f43196b84a3eec743c68fb60ec362
Michael Büsch <m@bues.ch>
2025-10-18 18:12:05 +0200

    avr24: Implement div in asm

--- avr-int24/src/asm_avr.rs
+++ avr-int24/src/asm_avr.rs
@@ -1,4 +1,4 @@
-use crate::raw::Int24Raw;
+use crate::raw::{Int24Raw, abs24, is_neg24};
 use core::arch::asm;
 
 #[inline(always)]
@@ -27,30 +27,63 @@ pub fn asm_mul24(mut a: Int24Raw, b: Int24Raw) -> Int24Raw {
 }
 
 #[inline(always)]
-pub fn asm_div24(mut a: Int24Raw, b: Int24Raw) -> Int24Raw {
+pub fn asm_div24(a: Int24Raw, b: Int24Raw) -> Int24Raw {
+    let res_neg = is_neg24(a) ^ is_neg24(b);
+    let mut a = abs24(a);
+    let b = abs24(b);
+
     unsafe {
         asm!(
-            "mov {r0_save}, r0",
-            "rcall __divmodpsi4",
-            "mov r0, {r0_save}",
+            "   ldi {loop}, 25",        // loop counter
 
-            inout("r22") a.0,
-            inout("r23") a.1,
-            inout("r24") a.2,
+            "   sub {rem0}, {rem0}",    // remainder = 0 and carry = 0
+            "   sub {rem1}, {rem1}",
+            "   sub {rem2}, {rem2}",
 
-            in("r18") b.0,
-            in("r19") b.1,
-            in("r20") b.2,
+            "1: rol {a0}",              // (dividend << 1) + carry
+            "   rol {a1}",
+            "   rol {a2}",
 
-            out("r21") _, // clobbered by __divmodpsi4
-            out("r25") _, // clobbered by __divmodpsi4
-            out("r26") _, // clobbered by __divmodpsi4
-            r0_save = out(reg) _, // clobbered by __divmodpsi4
+            "   dec {loop}",
+            "   breq 3f",               // loop counter == 0?
+
+            "   rol {rem0}",            // (remainder << 1) + dividend.23
+            "   rol {rem1}",
+            "   rol {rem2}",
+
+            "   sub {rem0}, {b0}",      // remainder -= divisor
+            "   sbc {rem1}, {b1}",
+            "   sbc {rem2}, {b2}",
+            "   brcs 2f",               // remainder was less than divisor?
+            "   sec",                   // result lsb = 1
+            "   rjmp 1b",
+            "2: add {rem0}, {b0}",
+            "   adc {rem1}, {b1}",
+            "   adc {rem2}, {b2}",
+            "   clc",                   // result lsb = 0
+            "   rjmp 1b",
+
+            "3:",
+
+            rem0 = out(reg) _,          // remainder
+            rem1 = out(reg) _,
+            rem2 = out(reg) _,
+
+            b0 = in(reg) b.0,           // divisor
+            b1 = in(reg) b.1,
+            b2 = in(reg) b.2,
+
+            a0 = inout(reg) a.0,        // dividend and quotient
+            a1 = inout(reg) a.1,
+            a2 = inout(reg) a.2,
+
+            loop = out(reg_upper) _,    // loop counter
 
             options(pure, nomem),
         );
     }
-    a
+
+    if res_neg { asm_neg24(a) } else { a }
 }
 
 #[inline(always)]
