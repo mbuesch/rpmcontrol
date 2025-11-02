3660caae8575194057617fdd2221a2b1e338491f
Michael Büsch <m@bues.ch>
2025-11-02 11:49:24 +0100

    Update avr-context

--- firmware/src/hw.rs
+++ firmware/src/hw.rs
@@ -2,28 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright (C) 2025 Michael Büsch <m@bues.ch>
 
-use avr_context::IrqCtx;
+use avr_context::define_isr;
 
 pub use attiny::{self as mcu, Peripherals};
 pub use avr_device::attiny861a as attiny;
 pub use avr_device::interrupt;
 
-macro_rules! define_isr {
-    ($name:ident, $handler:path) => {
-        #[avr_device::interrupt(attiny861a)]
-        fn $name() {
-            // SAFETY: We are inside of an interrupt handler.
-            // Therefore, it is safe to construct an `IrqCtx`.
-            let c = unsafe { IrqCtx::new() };
-            $handler(&c);
-        }
-    };
-}
+use crate::{
+    analog::irq_handler_ana_comp, exint::irq_handler_pcint, timer::irq_handler_timer1_compa,
+    usi_uart::irq_handler_usi_ovf,
+};
 
-define_isr!(PCINT, crate::exint::irq_handler_pcint);
-define_isr!(TIMER1_COMPA, crate::timer::irq_handler_timer1_compa);
-define_isr!(USI_OVF, crate::usi_uart::irq_handler_usi_ovf);
-define_isr!(ANA_COMP, crate::analog::irq_handler_ana_comp);
+define_isr!(attiny861a, PCINT, irq_handler_pcint);
+define_isr!(attiny861a, TIMER1_COMPA, irq_handler_timer1_compa);
+define_isr!(attiny861a, USI_OVF, irq_handler_usi_ovf);
+define_isr!(attiny861a, ANA_COMP, irq_handler_ana_comp);
 
 /// Do nothing for the duration of 3 CPU cycles.
 #[inline(always)]
