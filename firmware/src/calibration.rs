c7adfcffce8814988710c8aae743fb46c3fe8751
Michael Büsch <m@bues.ch>
2026-02-28 16:30:18 +0100

    cal: Update cal for real motor

--- firmware/src/calibration.rs
+++ firmware/src/calibration.rs
@@ -8,7 +8,7 @@ use crate::{
     freq::Freq, mains::MAINS_HALFWAVE_DUR_MS, pid::PidParams, system::rpm, temp::celsius,
     timer::RelLargeTimestamp,
 };
-use avr_q::{Q7p8, q7p8};
+use avr_q::{Q7p8, q7p8, Q15p8, q15p8};
 use curveipo::Curve;
 
 /// Initial delay after startup, before any actuation or monitoring is active.
@@ -16,14 +16,14 @@ pub const STARTUP_DELAY: RelLargeTimestamp = RelLargeTimestamp::from_millis(300)
 
 /// RPM PID parameters for normal operation.
 pub const RPMPID_PARAMS: PidParams = PidParams {
-    kp: q7p8!(const 5 / 1),
-    ki: q7p8!(const 1 / 8),
+    kp: q7p8!(const 8 / 5),
+    ki: q7p8!(const 3 / 32),
     kd: q7p8!(const 1 / 32),
 };
 
 /// RPM PID parameters for speedometer syncing.
 pub const RPMPID_PARAMS_SYNCING: PidParams = PidParams {
-    kp: q7p8!(const 5 / 1),
+    kp: q7p8!(const 2 / 1),
     ki: q7p8!(const 0),
     kd: q7p8!(const 0),
 };
@@ -33,8 +33,8 @@ pub const RPMPID_ILIM_NEG: Curve<Q7p8, (Q7p8, Q7p8), 4> = Curve::new([
     // (speedo, I-limit)
     (rpm!(0).0, q7p8!(const 0)),
     (rpm!(1000).0, q7p8!(const 0)),
-    (rpm!(1001).0, q7p8!(const -99)),
-    (rpm!(MAX_RPM).0, q7p8!(const -99)),
+    (rpm!(1001).0, q7p8!(const -10)),
+    (rpm!(MAX_RPM).0, q7p8!(const -10)),
 ]);
 
 /// Positive I-limit curve for the RPM PID controller.
@@ -42,8 +42,8 @@ pub const RPMPID_ILIM_POS: Curve<Q7p8, (Q7p8, Q7p8), 4> = Curve::new([
     // (speedo, I-limit)
     (rpm!(0).0, q7p8!(const 0)),
     (rpm!(1000).0, q7p8!(const 0)),
-    (rpm!(1001).0, q7p8!(const 99)),
-    (rpm!(MAX_RPM).0, q7p8!(const 99)),
+    (rpm!(1001).0, q7p8!(const 64)),
+    (rpm!(MAX_RPM).0, q7p8!(const 64)),
 ]);
 
 /// Substitute speedometer value curve for syncing, if the actual speedometer input is invalid.
@@ -66,7 +66,7 @@ pub const MOT_HARD_LIMIT: Freq = rpm!(MAX_RPM + 1500);
 pub const RPM_SYNC_THRES: Freq = rpm!(1000);
 
 /// Speedometer filter divider.
-pub const SPEED_FILTER_DIV: Q7p8 = q7p8!(const 2);
+pub const SPEED_FILTER_DIV: Q15p8 = q15p8!(const 100);
 
 /// Maximum amount of time we can live without a valid speedometer.
 pub const NO_SPEED_TIMEOUT: RelLargeTimestamp = RelLargeTimestamp::from_millis(100);
@@ -85,7 +85,7 @@ pub mod temp {
     pub const TEMP_LIMIT_LO: Q7p8 = celsius!(80);
 
     /// Temperature filter divider.
-    pub const TEMP_FILTER_DIV: Q7p8 = q7p8!(const 16);
+    pub const TEMP_FILTER_DIV: Q15p8 = q15p8!(const 16);
 
     /// Motor NTC temperature curve.
     pub const NTC_CURVE: Curve<Q7p8, (Q7p8, Q7p8), 7> = Curve::new([
